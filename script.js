46477da115fff826db56ef3e3d1e3b206b594570
Bill Matchett <129563150+SciSims3000@users.noreply.github.com>
2026-07-05 11:58:30 +1000

    Update script.js
    test1

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+fetch("activities/demo-science-7-1.json")
 
+.then(response => response.json())
+
+.then(activity => {
+
+    document.getElementById("activityTitle").textContent = activity.title;
+
+    document.getElementById("activityInfo").textContent =
+        activity.subject +
+        " • " +
+        activity.yearLevel +
+        " • " +
+        activity.topic;
+
+})
+
+.catch(error => {
+
+    console.error(error);
+
+});
